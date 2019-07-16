995206b000957cdaf14edf19c89e9ab18e1596c0
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-07-16 11:24:15 -0700

    Added constraints and listView

--- app/src/main/java/com/hazewritescode/friendzover/MainActivity.kt
+++ app/src/main/java/com/hazewritescode/friendzover/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.hazewritescode.friendzover
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -14,12 +19,8 @@ class MainActivity : AppCompatActivity() {
                 textMessage.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+            R.id.navigation_friends -> {
+                textMessage.setText("Friends")
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,8 +31,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val recipeListView = findViewById<ListView>(R.id.recipeListView)
+        recipeListView.adapter = RecipeListViewAdapter(this)
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private class RecipeListViewAdapter(context: Context): BaseAdapter() {
+
+        private val mContext: Context
+
+        init {
+            mContext = context
+
+        }
+
+
+        override fun getView(p0: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "Test"
+            return textView
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getItem(p0: Int): Any {
+            return "Test String"
+        }
+
     }
 }
